73e5d5ce41946e5385203c00c3508d7b52f307bb
AlecCoronadoGillen <77693927+AlecCoronadoGillen@users.noreply.github.com>
2021-02-04 19:02:16 -0600

    Update lab01.py

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -91,7 +91,7 @@ def gen_pattern(chars):
     newString = newString.center(len(mid), '.')
     finalThing = finalThing + "\n" + newString
     finalThing = newString + "\n" + finalThing
-  return finalThing
+  print(finalThing)
 
 def test4():
     tc = unittest.TestCase()
